bfc6bb53b0752633b8f247d57942a8dce82ea1b7
Kyle Repinski <repinski23@gmail.com>
2014-11-10 11:20:02 -0600

    New layout.

---
+++ tuna/toro/toro-vendor.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/samsung/tuna/toro-common/toro-common-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/tuna/toro/toro-vendor-blobs.mk)
+$(call inherit-product, vendor/samsung/tuna/tuna-vendor.mk)
+PRODUCT_PACKAGES += BIP
+PRODUCT_PACKAGES += IMSFramework
+PRODUCT_PACKAGES += RTN
+PRODUCT_PACKAGES += VZWAPNLib
+PRODUCT_PACKAGES += VZWAPNService
