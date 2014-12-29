7cddf46c5b10b9b5d3b6826b0b895dd06983ebf5
Kyle Repinski <repinski23@gmail.com>
2014-12-29 01:25:04 -0600

    toroplus: Fix build warning.

--- tuna/toroplus/toroplus-vendor-blobs.mk
+++ tuna/toroplus/toroplus-vendor-blobs.mk
@@ -1,5 +1,4 @@
 PRODUCT_COPY_FILES += \
-	vendor/samsung/tuna/toroplus/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
 	vendor/samsung/tuna/toroplus/proprietary/lib/libsyncml_core.so:system/lib/libsyncml_core.so \
 	vendor/samsung/tuna/toroplus/proprietary/lib/libsyncml_port.so:system/lib/libsyncml_port.so \
 	vendor/samsung/tuna/toroplus/proprietary/vendor/lib/libsec-ril_lte.so:system/vendor/lib/libsec-ril_lte.so
