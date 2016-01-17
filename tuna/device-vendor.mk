abe35faddf2d31ad3b6039ed94ef9255b755272f
root <justinjams@gmail.com>
2016-01-17 05:47:55 -0500

    Revert "Build Toro proprietary apps"
    This reverts commit 3645ccf4cf779053e34bf3cd8cd6de09226e1fb0.

--- tuna/device-vendor.mk
+++ tuna/device-vendor.mk
@@ -1,10 +1,1 @@
 $(call inherit-product, vendor/samsung/tuna/tuna-vendor-blobs.mk)
-
-# Build all product packages. We will remove the incompatible ones during variant setup.
-
-PRODUCT_PACKAGES += BIP
-PRODUCT_PACKAGES += IMSFramework
-PRODUCT_PACKAGES += RTN
-PRODUCT_PACKAGES += VZWAPNLib
-PRODUCT_PACKAGES += VZWAPNService
-
