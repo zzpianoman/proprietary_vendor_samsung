e09c6984654ec4f1db159e6110e9c90bb008906c
Ziyan <jaraidaniel@gmail.com>
2016-03-24 20:22:15 +0100

    tuna: remove DRM blobs

--- tuna/tuna-vendor-blobs.mk
+++ tuna/tuna-vendor-blobs.mk
@@ -6,14 +6,8 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_FOLDER)/firmware/ducati-m3.bin:system/vendor/firmware/ducati-m3.bin \
 	$(VENDOR_FOLDER)/firmware/mms144_ts_rev31.fw:system/vendor/firmware/mms144_ts_rev31.fw \
 	$(VENDOR_FOLDER)/firmware/mms144_ts_rev32.fw:system/vendor/firmware/mms144_ts_rev32.fw \
-	$(VENDOR_FOLDER)/lib/drm/libdrmwvmplugin.so:system/vendor/lib/drm/libdrmwvmplugin.so \
-	$(VENDOR_FOLDER)/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-	$(VENDOR_FOLDER)/lib/libdrmdecrypt.so:system/vendor/lib/libdrmdecrypt.so \
 	$(VENDOR_FOLDER)/lib/libfrsdk.so:system/vendor/lib/libfrsdk.so \
-	$(VENDOR_FOLDER)/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so \
-	$(VENDOR_FOLDER)/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
-	$(VENDOR_FOLDER)/lib/libwvm.so:system/vendor/lib/libwvm.so \
-	$(VENDOR_FOLDER)/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so
+	$(VENDOR_FOLDER)/lib/libinvensense_mpl.so:system/vendor/lib/libinvensense_mpl.so
 
 ifeq ($(TARGET_PREBUILT_CAMERA_CALIB),true)
 PRODUCT_COPY_FILES += \
